904b3bd97cb6b2c8dc76e0d99ddd9c80ae1422c6
Cédric Delfosse <cdelfosse@mandriva.com>
2007-03-22 13:41:36 +0000

    * Add icons for the side menu tabs of the mail module

--- mds/web/mail/mail/localSidebar.php
+++ mds/web/mail/mail/localSidebar.php
@@ -3,7 +3,7 @@
 $sidemenu= new SideMenu();
 
 $sidemenu->setClass("mail");
-$sidemenu->addSideMenuItem(new SideMenuItem(_T("Mail domain list"), "mail", "mail", "index"));
-$sidemenu->addSideMenuItem(new SideMenuItem(_T("Add a domain"), "mail", "mail", "add"));
+$sidemenu->addSideMenuItem(new SideMenuItem(_T("Mail domain list"), "mail", "mail", "index", "modules/mail/graph/img/mail_active.png", "modules/mail/graph/img/mail_inactive.png"));
+$sidemenu->addSideMenuItem(new SideMenuItem(_T("Add a domain"), "mail", "mail", "add", "modules/mail/graph/img/mailadd_active.png", "modules/mail/graph/img/mailadd_inactive.png"));
 
 ?>
