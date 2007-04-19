faafda071069b00308374e27470ceba1e200a6d3
Cédric Delfosse <cdelfosse@mandriva.com>
2007-04-19 09:12:35 +0000

    * Remove commented code

--- mds/web/network/network/edit.php
+++ mds/web/network/network/edit.php
@@ -1,4 +1,26 @@
-<?
+<?php
+/**
+ * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ *
+ * $Id$
+ *
+ * This file is part of LMC.
+ *
+ * LMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 require("modules/network/includes/network-xmlrpc.inc.php");
 require("modules/network/includes/network.inc.php");
 require("localSidebar.php");
@@ -20,8 +42,6 @@ else {
 $p = new PageGenerator();
 $p->setSideMenu($sidemenu);
 $p->displaySideMenu();
-
-//print_r(getAllZonesNetworkAddresses());
 
 if (isset($_POST["badd"])) {
     print_r($_POST);
