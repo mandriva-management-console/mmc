8f0848c381510bff6896f8e72b0b5d3aa922d4d6
Cédric Delfosse <cdelfosse@mandriva.com>
2007-04-19 09:13:55 +0000

    * Set action in service list according to service status

--- mds/web/network/network/services.php
+++ mds/web/network/network/services.php
@@ -1,4 +1,26 @@
-<?
+<?php
+/**
+ * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
+ *
+ * $Id$
+ *
+ * This file is part of LMC.
+ *
+ * LMC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LMC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LMC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 require("modules/network/includes/network-xmlrpc.inc.php");
 require("localSidebar.php");
 require("graph/navbar.inc.php");
@@ -29,19 +51,48 @@ if (isset($_GET["command"]) && isset($_GET["service"])) {
     }
     if (!isXMLRPCError()) {
         $result = _T("The service has been asked to") . "&nbsp;" . $command . ".";
-        $n = new NotifyWidget();
-	$n->flush();
-	$n->add("<div id=\"validCode\">$result</div>");
-	$n->setLevel(0);
-	$n->setSize(600);
+        new NotifyWidgetSuccess($result);
     }
 }
 
+/* Set the available action items according to the service status */
+$params = array();
+$actionsStart = array();
+$actionsStop = array();
+$actionsReload = array();
+$actionsLog = array();
+
+$startAction = new ActionItem(_T("Start service"),"services","start","command=start&amp;service");
+$stopAction = new ActionItem(_T("Stop service"),"services","stop","command=stop&amp;service");
+$reloadAction = new ActionItem(_T("Reload service"),"services","reload","command=reload&amp;service");
+$logAction = new ActionItem(_T("View log"),"services","afficher","command=logview&amp;service");
+$emptyAction = new EmptyActionItem();
+
 $status = array();
-if (dhcpService("status")) $status[] = _T("Started");
-else $status[] = _T("Stopped");
-if (dnsService("status")) $status[] = _T("Started");
-else $status[] = _T("Stopped");
+
+$actionsReload[] = $emptyAction;
+$actionsLog[] = $logAction;
+if (dhcpService("status")) {
+    $status[] = _T("Started");
+    $actionsStart[] = $emptyAction;
+    $actionsStop[] = $stopAction;
+} else {
+    $status[] = _T("Stopped");
+    $actionsStart[] = $startAction;
+    $actionsStop[] = $emptyAction;
+}
+
+$actionsLog[] = $logAction;
+$actionsReload[] = $reloadAction;
+if (dnsService("status")) {
+    $status[] = _T("Started");
+    $actionsStart[] = $emptyAction;
+    $actionsStop[] = $stopAction;
+} else {
+    $status[] = _T("Stopped");
+    $actionsStart[] = $startAction;
+    $actionsStop[] = $emptyAction;
+}
 
 $l = new ListInfos(array("DHCP", "DNS"), _T("Services"));
 $l->setName(_T("Network services status"));
@@ -49,10 +100,11 @@ $l->addExtraInfo($status, _T("Status"));
 $l->setParamInfo(array());
 $l->setTableHeaderPadding(1);
 $l->disableFirstColumnActionLink();
-$l->addActionItem(new ActionItem(_T("Start", "network"),"services&amp;command=start","start","service"));
-$l->addActionItem(new ActionItem(_T("Stop", "network"),"services&amp;command=stop","stop","service"));
-$l->addActionItem(new ActionItem(_T("Reload", "network"),"services&amp;command=reload","reload","service"));
-$l->addActionItem(new ActionItem(_T("View log", "network"),"services&amp;command=logview","afficher","service"));
+
+$l->addActionItemArray($actionsStart);
+$l->addActionItemArray($actionsStop);
+$l->addActionItemArray($actionsReload);
+$l->addActionItemArray($actionsLog);
 
 $l->display(0);
 
