b5617f9766002ba83b411539482f0d25bb7c2245
Cédric Delfosse <cdelfosse@mandriva.com>
2007-06-08 15:50:48 +0000

    * Bump module and debian package version to 2.0.0

--- mds/web/proxy/infoPackage.inc.php
+++ mds/web/proxy/infoPackage.inc.php
@@ -25,7 +25,7 @@
  * module declaration
  */
 $mod = new Module("proxy");
-$mod->setVersion("1.1.4");
+$mod->setVersion("2.0.0");
 $mod->setRevision("$Rev$");
 $mod->setDescription(_T("Web proxy"),"proxy");
 $mod->setAPIVersion('1:0:0');
