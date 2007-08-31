8be8300db3ecc91f06891bc89d452c73c42a484f
Cédric Delfosse <cdelfosse@mandriva.com>
2007-08-31 16:53:46 +0000

    * Use widget instead of HTML code

--- mds/web/network/network/zonemembers.php
+++ mds/web/network/network/zonemembers.php
@@ -26,63 +26,18 @@ require("localSidebar.php");
 require("graph/navbar.inc.php");
 
 $zone = $_GET["zone"];
-?>
+$ajax = new AjaxFilter("modules/network/network/ajaxZoneMembersFilter.php?zone=$zone");
+$ajax->display();
 
-<form name="Form" id="Form" action="#">
-
-    <div id="loader"><img id="loadimg" src="<?php echo $root; ?>img/common/loader.gif" alt="loader" class="loader"/></div>
-
-    <div id="searchSpan" class="searchbox" style="float: right;">
-    <img src="graph/search.gif" style="position:relative; top: 2px; float: left;" alt="search" /> <span class="searchfield"><input type="text" class="searchfieldreal" name="param" id="param" onkeyup="pushSearch(); return false;" />
-    <img src="graph/croix.gif" alt="suppression" style="position:relative; top : 3px;"
-    onclick="document.getElementById('param').value =''; pushSearch(); return false;" />
-    </span>
-    </div>
-
-    <script type="text/javascript">
-        document.getElementById('param').focus();
-
-
-                /**
-        * update div with user
-        */
-        function updateSearch() {
-            launch--;
-
-                if (launch==0) {
-                    new Ajax.Updater('container','modules/network/network/ajaxZoneMembersFilter.php?zone=<?=$zone?>&filter='+document.Form.param.value, { asynchronous:true, evalScripts: true});
-                }
-            }
-
-        /**
-        * provide navigation in ajax for user
-        */
-
-        function updateSearchParam(filter, start, end) {
-            new Ajax.Updater('container','modules/network/network/ajaxZoneMembersFilter.php?zone=<?=$zone?>&filter='+filter+'&start='+start+'&end='+end, { asynchronous:true, evalScripts: true});
-            }
-
-        /**
-        * wait 500ms and update search
-        */
-
-        function pushSearch() {
-            launch++;
-            setTimeout("updateSearch()",500);
-        }
-         
-        pushSearch();
-    </script>
-
-
-</form>
-
-<?
-$p = new PageGenerator(_T("Members of zone") . "&nbsp;" . $zone);
+$p = new PageGenerator(sprintf(_T("Members of zone %s"), $zone));
 $sidemenu->forceActiveItem("index");
 $p->setSideMenu($sidemenu);
 $p->display();
-?>
 
-<div id="container">
-</div>
+$ajax->displayDivToUpdate();
+
+$f = new Form();
+$f->addOnClickButton(_T("Add a host"), urlStr("network/network/addhost", array("zone" => $zone)));
+$f->display();
+
+?>
