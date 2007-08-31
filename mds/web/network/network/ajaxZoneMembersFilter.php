9a2596cd95e81348e9e377e544358338f21a71f1
Cédric Delfosse <cdelfosse@mandriva.com>
2007-08-31 16:55:02 +0000

    * Code cleanup * CNAME record support

--- mds/web/network/network/ajaxZoneMembersFilter.php
+++ mds/web/network/network/ajaxZoneMembersFilter.php
@@ -6,96 +6,78 @@ require("../../../includes/session.inc.php");
 require("../../../modules/network/includes/network-xmlrpc.inc.php");
 require ("../../../includes/PageGenerator.php");
 
-
-function print_ajax_nav($curstart, $curend, $items, $filter)
-{
-  $_GET["action"] = "index";
-  global $conf;
-
-  $max = $conf["global"]["maxperpage"];
-
-  echo '<form method="post" action="' . $PHP_SELF . '">';
-  echo "<ul class=\"navList\">\n";
-
-  if ($curstart == 0)
-    {
-      echo "<li class=\"previousListInactive\">"._("Previous")."</li>\n";
-    }
-  else
-    {
-      $start = $curstart - $max;
-      $end = $curstart - 1;
-      echo "<li class=\"previousList\"><a href=\"#\" onclick=\"updateSearchParam('$filter','$start','$end'); return false\";>"._("Previous")."</a></li>\n";
-    }
-
-  if (($curend + 1) >= count($items))
-    {
-      echo "<li class=\"nextListInactive\">"._("Next")."</li>\n";
-    }
-  else
-    {
-      $start = $curend + 1;
-      $end = $curend + $max;
-      echo "<li class=\"nextList\"><a href=\"#\" onclick=\"updateSearchParam('$filter','$start','$end'); return false\";>"._("Next")."</a></li>\n";
-    }
-
-  echo "</ul>\n";
-}
-
 $filter = $_GET["filter"];
 $zone = $_GET["zone"];
 $addresses = array();
 
-foreach(getZoneObjects($zone, "") as $dn => $entry) {
+$rrs = getZoneObjects($zone, "");
+
+/* Build a $hostname => IP array using A record only */
+$records = array();
+foreach($rrs as $dn => $entry) {
     $hostname = $entry[1]["relativeDomainName"][0];
-    $address = ip2long($entry[1]["aRecord"][0]); 
-    $addresses[$address] = $hostname;
+    if (isset($entry[1]["aRecord"])) {
+        $address = ip2long($entry[1]["aRecord"][0]);
+        $records[$hostname] = $address;
+    }
+}
+/* Complete the array using CNAME entries */
+$cnames = array();
+foreach($rrs as $dn => $entry) {
+    $alias = $entry[1]["relativeDomainName"][0];
+    if (isset($entry[1]["cNAMERecord"])) {
+        $cname = $entry[1]["cNAMERecord"][0];
+        $records[$alias] = $records[$cname];
+        $cnames[$alias] = $cname;
+    }
+}
+asort($records);
+
+$params = array();
+$hosts = array();
+$ips = array();
+$actionsDel = array();
+$actionsMod = array();
+$delHostAction = new ActionPopupItem(_T("Delete host (A record)", "network"),"deletehost","supprimer","", "network", "network");
+$delAliasAction = new ActionPopupItem(_T("Delete alias (CNAME record)", "network"),"deletehost","supprimer","", "network", "network");
+$emptyAction = new EmptyActionItem();
+$editAction = new ActionItem(_T("edit record", "network"),"edithost","edit","", "network", "network");
+foreach($records as $host => $ip) {
+    $ipstr = long2ip($ip);
+    $display = True;
     if ($filter) {
         /* Don't display a host if filtered */
         if (
-            (strpos($hostname, $filter) === False)
-            && (strpos($entry[1]["aRecord"][0], $filter) === False)
+            (strpos($host, $filter) === False)
+            && (strpos($ipstr, $filter) === False)
             ) {
-            unset($addresses[$address]);
-        }        
+            $display = False;
+        }
+    }
+    if ($display) {
+        $ips[] = $ipstr;
+        $params[] = array("host" => $host, "zone" => $zone);
+        if (in_array($host, array_keys($cnames))) {
+            $host = "$host (CNAME " . $cnames[$host] . ")";
+            $actionsMod[] = $emptyAction;            
+            $actionsDel[] = $delAliasAction;
+        } else {
+            $actionsMod[] = $editAction;
+            $actionsDel[] = $delHostAction;
+        }
+        $hosts[] = $host;        
     }
 }
 
-ksort($addresses);
-$sorted = array();
-$params = array();
-foreach($addresses as $ip => $host) {
-    $sorted[] = long2ip($ip);
-    $params[] = array("host" => $host, "zone" => $zone);
-}
-
-if (isset($_GET["start"])) {
-    $start = $_GET["start"];
-    $end = $_GET["end"];
-} else {
-    $start = 0;
-    if (count($addresses) > 0) {
-        $end = $conf["global"]["maxperpage"] - 1;
-    } else {
-        $end = 0;
-    }
-}
-
-print_ajax_nav($start, $end, $addresses, $filter);
-
-$n = new ListInfos($sorted, _T("IP address", "network"));
+$n = new ListInfos($ips, _T("IP address", "network"));
 $n->setTableHeaderPadding(1);
-$n->addExtraInfo(array_values($addresses), _T("Host name", "network"));
+$n->setNavBar(new AjaxNavBar(count($ips), $filter));
+$n->addExtraInfo($hosts, _T("Host name", "network"));
 $n->setName(_T("Host", "network"));
 $n->setParamInfo($params);
 $n->disableFirstColumnActionLink();
-
-$n->addActionItem(new ActionPopupItem(_T("Delete host", "network"),"deletehost","supprimer","", "network", "network"));
-
-$n->display(0);
-
-print_ajax_nav($start, $end, $addresses, $filter);
+$n->addActionItemArray($actionsMod);
+$n->addActionItemArray($actionsDel);
+$n->display();
 
 ?>
-
-<input type="button" class="btnPrimary" value="<?= _T("Add a host", "network"); ?>" onclick="location.href='main.php?module=network&submod=network&action=addhost&zone=<?= $zone; ?>';"/>
