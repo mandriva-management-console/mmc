9b0b928486c8bd80b49b45fa90232cd88e0877f1
Cédric Delfosse <cdelfosse@mandriva.com>
2007-09-03 15:27:06 +0000

    * Code cleanup

--- mds/web/network/network/subnetmembers.php
+++ mds/web/network/network/subnetmembers.php
@@ -26,62 +26,14 @@ require("localSidebar.php");
 require("graph/navbar.inc.php");
 
 $subnet = $_GET["subnet"];
-?>
 
-<form name="Form" id="Form" action="#">
+$ajax = new AjaxFilter("modules/network/network/ajaxSubnetMembersFilter.php?subnet=$subnet");
+$ajax->display();
 
-    <div id="loader"><img id="loadimg" src="<?php echo $root; ?>img/common/loader.gif" alt="loader" class="loader"/></div>
-
-    <div id="searchSpan" class="searchbox" style="float: right;">
-    <img src="graph/search.gif" style="position:relative; top: 2px; float: left;" alt="search" /> <span class="searchfield"><input type="text" class="searchfieldreal" name="param" id="param" onkeyup="pushSearch(); return false;" />
-    <img src="graph/croix.gif" alt="suppression" style="position:relative; top : 3px;"
-    onclick="document.getElementById('param').value =''; pushSearch(); return false;" />
-    </span>
-    </div>
-
-    <script type="text/javascript">
-        document.getElementById('param').focus();
-
-
-                /**
-        * update div with user
-        */
-        function updateSearch() {
-            launch--;
-
-                if (launch==0) {
-                    new Ajax.Updater('container','modules/network/network/ajaxSubnetMembersFilter.php?subnet=<?=$subnet?>&filter='+document.Form.param.value, { asynchronous:true, evalScripts: true});
-                }
-            }
-
-        /**
-        * provide navigation in ajax for user
-        */
-
-        function updateSearchParam(filter, start, end) {
-            new Ajax.Updater('container','modules/network/network/ajaxSubnetMembersFilter.php?subnet=<?=$subnet?>&filter='+filter+'&start='+start+'&end='+end, { asynchronous:true, evalScripts: true});
-            }
-
-        /**
-        * wait 500ms and update search
-        */
-
-        function pushSearch() {
-            launch++;
-            setTimeout("updateSearch()",500);
-        }
-         
-        pushSearch();
-    </script>
-
-</form>
-
-<?
 $p = new PageGenerator(_T("Members of DHCP subnet") . "&nbsp;" . $subnet);
 $sidemenu->forceActiveItem("subnetindex");
 $p->setSideMenu($sidemenu);
 $p->display();
-?>
 
-<div id="container">
-</div>
+$ajax->displayDivToUpdate();
+?>
