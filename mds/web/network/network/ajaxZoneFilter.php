00d50de5aa03bcdf15b662a1b135b81f6130f176
Cédric Delfosse <cdelfosse@mandriva.com>
2007-09-03 15:49:19 +0000

    * Code cleanup

--- mds/web/network/network/ajaxZoneFilter.php
+++ mds/web/network/network/ajaxZoneFilter.php
@@ -6,42 +6,6 @@ require("../../../includes/session.inc.php");
 require("../../../modules/network/includes/network-xmlrpc.inc.php");
 require ("../../../includes/PageGenerator.php");
 
-
-function print_ajax_nav($curstart, $curend, $items, $filter)
-{
-  $_GET["action"] = "index";
-  global $conf;
-
-  $max = $conf["global"]["maxperpage"];
-
-  echo '<form method="post" action="' . $PHP_SELF . '">';
-  echo "<ul class=\"navList\">\n";
-
-  if ($curstart == 0)
-    {
-      echo "<li class=\"previousListInactive\">"._("Previous")."</li>\n";
-    }
-  else
-    {
-      $start = $curstart - $max;
-      $end = $curstart - 1;
-      echo "<li class=\"previousList\"><a href=\"#\" onclick=\"updateSearchParam('$filter','$start','$end'); return false\";>"._("Previous")."</a></li>\n";
-    }
-
-  if (($curend + 1) >= count($items))
-    {
-      echo "<li class=\"nextListInactive\">"._("Next")."</li>\n";
-    }
-  else
-    {
-      $start = $curend + 1;
-      $end = $curend + $max;
-      echo "<li class=\"nextList\"><a href=\"#\" onclick=\"updateSearchParam('$filter','$start','$end'); return false\";>"._("Next")."</a></li>\n";
-    }
-
-  echo "</ul>\n";
-}
-
 $filter = $_GET["filter"];
 $zones = array();
 $count = array();
@@ -69,21 +33,8 @@ foreach($zones as $zone => $infos) {
     else $reverses[] = $reverse[0] . ".";
 }
 
-if (isset($_GET["start"])) {
-    $start = $_GET["start"];
-    $end = $_GET["end"];
-} else {
-    $start = 0;
-    if (count($zones) > 0) {
-        $end = $conf["global"]["maxperpage"] - 1;
-    } else {
-        $end = 0;
-    }
-}
-
-print_ajax_nav($start, $end, $zones, $filter);
-
 $n = new ListInfos(array_keys($zones), _T("DNS zones", "network"));
+$n->setNavBar(new AjaxNavBar(count($zones), $filter));
 $n->setAdditionalInfo($count);
 $n->first_elt_padding = 1;
 $n->addExtraInfo($reverses, _T("Network prefix", "network"));
@@ -95,8 +46,6 @@ $n->addActionItem(new ActionItem(_T("Edit zone", "network"),"edit","edit","zone"
 $n->addActionItem(new ActionItem(_T("Add host", "network"),"addhost","addhost","zone", "network", "network"));
 $n->addActionItem(new ActionPopupItem(_T("Delete zone", "network"),"delete","supprimer","zone", "network", "network"));
 
-$n->display(0);
-
-print_ajax_nav($start, $end, $zones, $filter);
+$n->display();
 
 ?>
