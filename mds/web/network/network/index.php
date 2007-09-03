b00b0f4a7030901eb89408b46b08b7743a22cd59
Cédric Delfosse <cdelfosse@mandriva.com>
2007-09-03 15:20:18 +0000

    * Code cleanup

--- mds/web/network/network/index.php
+++ mds/web/network/network/index.php
@@ -24,62 +24,14 @@
 require("modules/network/includes/network-xmlrpc.inc.php");
 require("localSidebar.php");
 require("graph/navbar.inc.php");
-?>
 
-<form name="Form" id="Form" action="#">
+$ajax = new AjaxFilter("modules/network/network/ajaxZoneFilter.php");
+$ajax->display();
 
-    <div id="loader"><img id="loadimg" src="<?php echo $root; ?>img/common/loader.gif" alt="loader" class="loader"/></div>
-
-    <div id="searchSpan" class="searchbox" style="float: right;">
-    <img src="graph/search.gif" style="position:relative; top: 2px; float: left;" alt="search" /> <span class="searchfield"><input type="text" class="searchfieldreal" name="param" id="param" onkeyup="pushSearch(); return false;" />
-    <img src="graph/croix.gif" alt="suppression" style="position:relative; top : 3px;"
-    onclick="document.getElementById('param').value =''; pushSearch(); return false;" />
-    </span>
-    </div>
-
-    <script type="text/javascript">
-        document.getElementById('param').focus();
-
-
-                /**
-        * update div with user
-        */
-        function updateSearch() {
-            launch--;
-
-                if (launch==0) {
-                    new Ajax.Updater('container','modules/network/network/ajaxZoneFilter.php?filter='+document.Form.param.value, { asynchronous:true, evalScripts: true});
-                }
-            }
-
-        /**
-        * provide navigation in ajax for user
-        */
-
-        function updateSearchParam(filter, start, end) {
-            new Ajax.Updater('container','modules/network/network/ajaxZoneFilter.php?filter='+filter+'&start='+start+'&end='+end, { asynchronous:true, evalScripts: true});
-            }
-
-        /**
-        * wait 500ms and update search
-        */
-
-        function pushSearch() {
-            launch++;
-            setTimeout("updateSearch()",500);
-        }
-         
-        pushSearch();
-    </script>
-
-
-</form>
-
-<?
 $p = new PageGenerator(_T("DNS zones management"));
 $p->setSideMenu($sidemenu);
 $p->display();
+
+$ajax->displayDivToUpdate();
 ?>
 
-<div id="container">
-</div>
