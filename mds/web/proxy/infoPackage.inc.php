d57dcaa2ba25147d03f78e313327c7ea1097da99
Cédric Delfosse <cdelfosse@mandriva.com>
2007-09-10 13:06:41 +0000

    * Bump to version 3.0.0

--- mds/web/proxy/infoPackage.inc.php
+++ mds/web/proxy/infoPackage.inc.php
@@ -26,7 +26,7 @@
  * module declaration
  */
 $mod = new Module("proxy");
-$mod->setVersion("2.0.0");
+$mod->setVersion("3.0.0");
 $mod->setRevision("$Rev$");
 $mod->setDescription(_T("Web proxy"),"proxy");
 $mod->setAPIVersion('1:0:0');
