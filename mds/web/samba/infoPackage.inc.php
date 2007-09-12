ba761565caa4ff0d768b0a37c7788120f19f94ad
Cédric Delfosse <cdelfosse@mandriva.com>
2007-09-12 08:34:59 +0000

    * Change version to 2.1.0

--- mds/web/samba/infoPackage.inc.php
+++ mds/web/samba/infoPackage.inc.php
@@ -26,7 +26,7 @@
  * module declaration
  */
 $mod = new Module("samba");
-$mod->setVersion("3.0.0");
+$mod->setVersion("2.1.0");
 $mod->setRevision("$Rev$");
 $mod->setDescription(_T("SAMBA service"),"samba");
 $mod->setAPIVersion("4:1:3");
