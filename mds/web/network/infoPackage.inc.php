38f6287a93ad8d89ce2bccee7fac8055297b38d0
Cédric Delfosse <cdelfosse@mandriva.com>
2007-09-12 08:37:13 +0000

    * Change version to 2.1.0

--- mds/web/network/infoPackage.inc.php
+++ mds/web/network/infoPackage.inc.php
@@ -26,7 +26,7 @@
  * module declaration
  */
 $mod = new Module("network");
-$mod->setVersion("3.0.0");
+$mod->setVersion("2.1.0");
 $mod->setRevision("$Rev$");
 $mod->setDescription(_T("Network management", "network"));
 $mod->setAPIVersion('0:0:0');
