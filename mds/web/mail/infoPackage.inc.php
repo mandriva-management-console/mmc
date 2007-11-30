6111dd3fcfe527bce75b5517578f022a4164e124
Cédric Delfosse <cdelfosse@mandriva.com>
2007-11-30 10:04:29 +0000

    * Update version

--- mds/web/mail/infoPackage.inc.php
+++ mds/web/mail/infoPackage.inc.php
@@ -31,7 +31,7 @@
 require_once("modules/mail/includes/mail-xmlrpc.php");
 
 $mod = new Module("mail");
-$mod->setVersion("2.1.0");
+$mod->setVersion("2.2.0");
 $mod->setRevision("$Rev$");
 $mod->setDescription(_T("Mail service","mail"));
 $mod->setAPIVersion("5:1:3");
