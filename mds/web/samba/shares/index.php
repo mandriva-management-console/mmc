b0f2043cec44c6b636f2bc0cab5eda3f6365a7e5
Cédric Delfosse <cdelfosse@mandriva.com>
2008-01-07 09:37:54 +0000

    * Code cleanup

--- mds/web/samba/shares/index.php
+++ mds/web/samba/shares/index.php
@@ -32,107 +32,25 @@ require("graph/navbar.inc.php");
 
 global $conf;
 
-if (!isset($_GET["items"]))
-{
-  if (!isset($_POST["start"]))
-  {
-    $shares = get_shares_detailed();
-    $start = 0;
-
-    if (count($shares) > 0)
-      {
-	$end = $conf["global"]["maxperpage"] - 1;
-      }
-    else
-      {
-	$end = 0;
-      }
-  }
-}
-else
-{
-  $shares = unserialize(base64_decode(urldecode($_GET["items"])));
-}
-if (isset($_GET["start"]))
-{
-$start = $_GET["start"];
-$end = $_GET["end"];
+$shares = get_shares_detailed();
+$sharesName = array();
+$sharesComment = array();
+foreach($shares as $share) {
+    if (!in_array($share[0], $protectedShare)) {
+        $sharesName[] = $share[0];
+        $sharesComment[] = $share[1];
+    }
 }
 
 $p = new PageGenerator(_T("Shares"));
 $p->setSideMenu($sidemenu);
 $p->display();
 
-print_nav($start, $end, $shares);
-global $maxperpage; //definition globale
-?>
+$l = new ListInfos($sharesName, _T("Shares"));
+$l->addExtraInfo($sharesComment);
+$l->addActionItem(new ActionItem(_T("Edit"),"details","edit","share"));
+$l->addActionItem(new ActionPopupItem(_T("Delete"),"delete","delete","share"));
+$l->addActionItem(new ActionPopupItem(_T("Archive"),"backup","backup","share"));
+$l->display();
 
-<p class="listInfos">
-
-<?php
-
-global $maxperpage;
-printf(_T("Shares <strong>%s</strong>
-to <strong>%s</strong>
-on a total of <strong>%s</strong>
-(page %s on "),min(($start + 1), count($shares)),min(($end + 1), count($shares)),count($shares),sprintf("%.0f", ($end + 1) / $maxperpage));
-
-?>
-<?php
- $pages = count($shares) / $maxperpage;
- if ((count($shares) % $maxperpage > 0) && (count($shares) > $maxperpage))
-   {
-     $pages++;
-   }
- if ((count($shares) > 0) && ($pages < 1))
-   {
-     $pages = 1;
-   }
- printf("%.0f", $pages);
-?>
-)
-</p>
-
-<table border="1" cellspacing="0" cellpadding="5">
-
-
-<?php
-
-for ($idx = $start;
-     ($idx < count($shares)) && ($idx <= $end);
-     $idx++)
-{
-  if (($start - $idx) % 2)
-    {
-      echo "<tr>";
-    }
-  else
-    {
-      echo "<tr class=\"alternate\">";
-    }
-
-  echo "<td class=\"shareName\">".$shares[$idx][0]."</td>";
-  echo "<td>".$shares[$idx][1]."</td>";
-  echo "<td class=\"shareAction\">";
-  echo "<ul class=\"action\">";
-  echo "<li class=\"edit\">";
-  if(array_search($shares[$idx][0],$protectedShare)==null) {
-    echo "<a title=\"Propriétés\" href=\"main.php?module=samba&submod=shares&action=details&share=".urlencode($shares[$idx][0])."\">.</a></li>";
-
-      $a = new ActionPopupItem(_T("Delete"),"delete","delete","share");
-      $a->display(urlencode($shares[$idx][0]));
-
-    }
-  $a = new ActionPopupItem(_T("Archive"),"backup","backup","share");
-  $a->display(urlencode($shares[$idx][0]));
-  echo "</ul>";
-  echo "</td>";
-  echo "</tr>\n";
-}
-?>
-
-</table>
-
-<?php
-print_nav($start, $end, $shares);
 ?>
