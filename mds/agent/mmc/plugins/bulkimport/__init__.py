f84ee4bcb2a0139ba2b8f3c742d0f3d01ea5b3f3
Cédric Delfosse <cdelfosse@mandriva.com>
2010-03-30 14:51:23 +0000

    * Set svn:keywords Id Rev * Update license * Remove useless import

--- mds/agent/mmc/plugins/bulkimport/__init__.py
+++ mds/agent/mmc/plugins/bulkimport/__init__.py
@@ -2,7 +2,7 @@
 #
 # (c) 2009 Open Systems Specilists - Glen Ogilvie
 #
-# $Id: __init__.py 743 2008-12-15 14:20:35Z cdelfosse $
+# $Id$
 #
 # This file is part of Mandriva Management Console (MMC).
 #
@@ -17,28 +17,17 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with MMC; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# along with MMC.  If not, see <http://www.gnu.org/licenses/>.
 
-""" Bulkimport """
-
-import socket
-import ldap
-import logging
-import os
-import os.path
-import grp
-
-from mmc.plugins.base import ldapUserGroupControl
-from mmc.support.config import *
-from mmc.support import mmctools
-import mmc
+"""
+MDS bulk import plugin for the MMC agent.
+"""
 
 INI = "/etc/mmc/plugins/bulkimport.ini"
 
 VERSION = "0.0.2"
 APIVERSION = "1:0:0"
-REVISION = int("$Rev: 1 $".split(':')[1].strip(' $'))
+REVISION = int("$Rev$".split(':')[1].strip(' $'))
 
 def getVersion(): return VERSION
 def getApiVersion(): return APIVERSION
