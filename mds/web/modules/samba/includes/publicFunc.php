2baf690e95b6e3362264d60751bb022d9759fe81
Jean-Philippe Braun <jpbraun@mandriva.com>
2011-08-01 13:19:26 +0000

    * mds: fix samba module public functions

--- mds/web/modules/samba/includes/publicFunc.php
+++ mds/web/modules/samba/includes/publicFunc.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * (c) 2004-2007 Linbox / Free&ALter Soft, http://linbox.com
- * (c) 2007-2008 Mandriva, http://www.mandriva.com/
+ * (c) 2007-2011 Mandriva, http://www.mandriva.com/
  *
  * $Id$
  *
@@ -21,16 +21,9 @@
  * along with MMC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-?>
-<?
 
 include ("user-xmlrpc.inc.php");
 
-function _samba_infoUser($userObjClass) {
-    // FIXME: test should be case insensitive
-    if (array_search("sambaSamAccount",$userObjClass) || array_search("sambaSAMAccount",$userObjClass))
-        print "sambaAccount ";
-}
 
 function _samba_delUser($uid) {
     if (hasSmbAttr($uid)) rmSmbAttr($uid);
@@ -51,32 +44,37 @@ function _samba_changeUserPasswd($paramsArr) {
 function _samba_changeUserPrimaryGroup($uid, $group) {
     if (hasSmbAttr($uid)) xmlCall("samba.changeUserPrimaryGroup",array($uid, $group));
 }
- 
 
-function _samba_changeUser($FH) {
+
+/**
+ * Function called for changing user attributes
+ * @param $FH FormHandler of the page
+ * @param $mode add or edit mode
+ */
+function _samba_changeUser($FH, $mode) {
 
     global $error;
+    global $result;
     if ($error) return -1;
 
     # check users profiles setup
-    $hasProfiles = xmlCall("samba.isProfiles");
+    $globalProfiles = xmlCall("samba.isProfiles");
 
-    if  (hasSmbAttr($FH->getPostValue("nlogin"))) { //if it is an smbUser
+    # Already existing SAMBA user
+    if  (hasSmbAttr($FH->getPostValue("uid"))) {
+        # Remove atrributes
         if (!$FH->getPostValue("isSamba")) {
-            // Removing all SAMBA attributes
-            rmSmbAttr($FH->getPostValue("nlogin"));
-            global $result;
+            rmSmbAttr($FH->getPostValue("uid"));
             $result .= _T("Samba attributes deleted.","samba")."<br />";
-        } else {
-            // Change SAMBA attributes
-            /*
-             * Remove passwords from the $_POST array coming from the add/edit user page
-             * because we don't want to send them again via RPC.
-             */
+        }
+        # Update attributes
+        else {
+            # Remove passwords from the $_POST array coming from the add/edit user page
+            # because we don't want to send them again via RPC.
             $FH->delPostValue("pass");
             $FH->delPostValue("confpass");
 
-            // format samba attributes
+            // Format samba attributes
             if($FH->isUpdated("sambaPwdLastSet")) {
                 if($FH->getValue("sambaPwdLastSet") == "on") {
                     // force user to change password
@@ -88,7 +86,7 @@ function _samba_changeUser($FH) {
             }
             if($FH->isUpdated("sambaPwdCanChange")) {
                 if($FH->getValue("sambaPwdCanChange") == "on") {
-                    // del this attribute
+                    // Remove this attribute
                     $FH->setValue("sambaPwdCanChange", "");
                 }
                 else {
@@ -98,7 +96,7 @@ function _samba_changeUser($FH) {
             }
             // account expiration
             if($FH->isUpdated("sambaKickoffTime")) {
-                $datetime = $FH->getValue("sambaKickoffTime"); 
+                $datetime = $FH->getValue("sambaKickoffTime");
                 // 2010-09-23 18:32:00
                 if (strlen($datetime) == 19) {
                     $timestamp = mktime(substr($datetime, -8, 2), substr($datetime, -5, 2), substr($datetime, -2, 2), substr($datetime, 5, 2), substr($datetime, 8, 2), substr($datetime, 0, 4));
@@ -109,65 +107,50 @@ function _samba_changeUser($FH) {
                     $FH->setValue("sambaKickoffTime", "");
                 }
             }
-            if($FH->isUpdated("hasProfile")) {
-                if($FH->getValue("hasProfile") == "on" && !$hasProfiles) {
-                    $FH->setValue("sambaProfilePath", $FH->getPostValue("sambaProfilePath"));
-                }
-                else {
-                    $FH->setValue("sambaProfilePath", "");
-                }
+            // Network profile path
+            if($FH->isUpdated("sambaProfilePath") && !$globalProfiles) {
+                $FH->setValue("sambaProfilePath", $FH->getPostValue("sambaProfilePath"));
             }
-            # FIXME !
-            # see #112
-            # need to find a way to disable roaming profile for user when logon path is set
-            /*
-                else if(($FH->getValue("sambaProfilePath") == "on" && $globalProfiles) || !$globalProfiles) {
-                    # sambaProfilePath is useless
-                    $FH->setValue("sambaProfilePath", "");
-                }
-                else {
-                    # desactivate profile for this user
-                    $FH->setValue("sambaProfilePath", '%USERPROFILE%');
-                }
-            }*/
-            
+            else {
+                $FH->setValue("sambaProfilePath", "");
+            }
+
             // change attributes
-            changeSmbAttr($FH->getPostValue("nlogin"), $FH->getValues());
-            
-            if (isEnabledUser($FH->getPostValue("nlogin"))) {
+            changeSmbAttr($FH->getPostValue("uid"), $FH->getValues());
+
+            if (isEnabledUser($FH->getPostValue("uid"))) {
                 if ($FH->getPostValue('isSmbDesactive')) {
-                    smbDisableUser($FH->getPostValue("nlogin"));
+                    smbDisableUser($FH->getPostValue("uid"));
                 }
             } else {
                 if (!$FH->getPostValue('isSmbDesactive')) {
-                    smbEnableUser($FH->getPostValue("nlogin"));
+                    smbEnableUser($FH->getPostValue("uid"));
                 }
             }
-            if (isLockedUser($FH->getPostValue("nlogin"))) {
+            if (isLockedUser($FH->getPostValue("uid"))) {
                 if (!$FH->getPostValue('isSmbLocked')) {
-                    smbUnlockUser($FH->getPostValue("nlogin"));
+                    smbUnlockUser($FH->getPostValue("uid"));
                 }
             } else {
                 if ($FH->getPostValue('isSmbLocked')) {
-                    smbLockUser($FH->getPostValue("nlogin"));
+                    smbLockUser($FH->getPostValue("uid"));
                 }
             }
         }
     }
     else { //if not have smb attributes
         if ($FH->getPostValue("isSamba")) {
-            global $result;
-            $result.=_T("Samba attributes added.","samba")."<br />";
+
             # if the user password doesn't match the pwd policies
             # we set a random password for the samba user
             if($FH->getValue("randomSmbPwd") == 1) {
                 $FH->setPostValue("pass", uniqid(rand(), true));
             }
-            addSmbAttr($FH->getPostValue("nlogin"),$FH->getPostValue("pass"));
+            # Add SAMBA attributes
+            addSmbAttr($FH->getPostValue("uid"),$FH->getPostValue("pass"));
+            $result .= _T("Samba attributes added.","samba")."<br />";
 
-            // format samba attributes
-            // FIXME
-            // duplicate with edit phase
+            // Format samba attributes
             if($FH->getPostValue("sambaPwdLastSet") == "on") {
                 $FH->setPostValue("sambaPwdLastSet", "0");
             }
@@ -177,12 +160,15 @@ function _samba_changeUser($FH) {
             else {
                 $FH->setPostValue("sambaPwdCanChange", "9999999999");
             }
-            // account expiration
+            // Account expiration
             if($FH->isUpdated("sambaKickoffTime")) {
-                $datetime = $FH->getValue("sambaKickoffTime"); 
+                $datetime = $FH->getValue("sambaKickoffTime");
                 // 2010-09-23 18:32:00
                 if (strlen($datetime) == 19) {
-                    $timestamp = mktime(substr($datetime, -8, 2), substr($datetime, -5, 2), substr($datetime, -2, 2), substr($datetime, 5, 2), substr($datetime, 8, 2), substr($datetime, 0, 4));
+                    $timestamp = mktime(substr($datetime, -8, 2),
+                        substr($datetime, -5, 2), substr($datetime, -2, 2),
+                        substr($datetime, 5, 2), substr($datetime, 8, 2),
+                        substr($datetime, 0, 4));
                     $FH->setPostValue("sambaKickoffTime", "$timestamp");
                 }
                 // not a valid value
@@ -190,41 +176,38 @@ function _samba_changeUser($FH) {
                     $FH->setPostValue("sambaKickoffTime", "");
                 }
             }
-            
-            if($FH->getPostValue("hasProfile") == "on" && !$globalProfiles) {
-                $smbconf = xmlCall("samba.getSmbInfo");
-                $FH->setPostValue("sambaProfilePath", "\\\\".$smbconf["netbios name"]."\\".$FH->getPostValue("nlogin")."\\profile");
-            }
-            else {
+            // Network profile
+            // Clear profile path if global profiles are on
+            if(!$FH->getPostValue("hasProfile") == "on" || $globalProfiles) {
                 $FH->setPostValue("sambaProfilePath", "");
             }
 
-            changeSmbAttr($FH->getPostValue("nlogin"), $FH->getPostValues());
+            changeSmbAttr($FH->getPostValue("uid"), $FH->getPostValues());
         }
     }
 }
 
-function _samba_verifInfo($postArr) {
+/**
+ * Function called before changing user attributes
+ * @param $FH FormHandler of the page
+ * @param $mode add or edit mode
+ */
+function _samba_verifInfo($FH, $mode) {
 
-    if (isset($postArr["isSamba"])) {
-        if (!hasSmbAttr($postArr["nlogin"])) {
-            if ($postArr["pass"]=="") { //if we not precise password
+    if ($FH->getPostValue("isSamba")) {
+        if (!hasSmbAttr($FH->getPostValue("uid"))) {
+            if (!$FH->getValue("pass")) { // if we not precise password
                 global $error;
                 $error.= _T("You must reenter your password.","samba")."<br />\n";
                 setFormError("pass");
                 return -1;
             }
         }
-        $drive = "";
-        if (isset($postArr["sambaHomeDrive"]))
-            $drive = trim($postArr["sambaHomeDrive"]);
+        $drive = $FH->getValue("sambaHomeDrive");
         if ($drive != "") {
-
             // Check that the SAMBA home drive is a correct drive letter
             // "X:", "U:", etc.
             $err = False;
-            //if (strlen($drive) != 2) $err = True;
-            //elseif ($drive{1} != ":") $err = True;
             if (!preg_match("/^[C-Z]:$/", $drive)) {
                 global $error;
                 $error .= _T("Invalid network drive.","samba")."<br />\n";
@@ -235,140 +218,130 @@ function _samba_verifInfo($postArr) {
     }
 }
 
-function _samba_baseEdit($ldapArr,$postArr) {
+/**
+ * Form on user edit page
+ * @param $FH FormHandler of the page
+ * @param $mode add or edit mode
+ */
+function _samba_baseEdit($FH, $mode) {
 
     // default values
     $checked = "checked";
     $hasSmb = true;
-    
+    $show = true;
+
     // get smb config info
     $smbInfo = xmlCall("samba.getSmbInfo", null);
 
     // fetch ldap updated info if we can
-    if (isset($ldapArr["uid"][0])) {
-        $uid = $ldapArr["uid"][0];
-        if (!hasSmbAttr($ldapArr["uid"][0])) {
+    if ($mode == 'edit') {
+        $uid = $FH->getArrayOrPostValue("uid");
+        if (!hasSmbAttr($uid)) {
             $checked = "";
             $hasSmb = false;
+            $show = false;
+        }
+        // show Samba plugin in case of error
+        if ($FH->getValue("isSamba") == "on") {
+            $checked = "checked";
+            $show = true;
         }
     }
 
-    // if we update a user but error when updating
-    if (isset($postArr["buser"])) {
-        if (isset($postArr["isSamba"])) {
-            $checked = "checked";
-            $hasSmb = true;
-        } else {
-            $checked = "";
-            $hasSmb = false;
-        }
-    }
-    
-    $f = new DivForModule(_T("Samba user properties","samba"), "#EFE");    
+    $f = new DivForModule(_T("Samba user properties","samba"), "#EFE");
     $f->push(new Table());
-    
+
     $f->add(
         new TrFormElement(_T("SAMBA access","samba"), new CheckboxTpl("isSamba")),
         array("value"=>$checked, "extraArg"=>'onclick="toggleVisibility(\'smbdiv\');"')
     );
 
     $f->pop();
-    
+
     $smbdiv = new Div(array("id" => "smbdiv"));
-    $smbdiv->setVisibility($hasSmb);
+    $smbdiv->setVisibility($show);
 
     $f->push($smbdiv);
-    $f->push(new Table());    
+    $f->push(new Table());
+
+    if ($hasSmb && userPasswdHasExpired($uid)) {
+        $formElt = new HiddenTpl("userPasswdHasExpired");
+        $f->add(
+            new TrFormElement(_("WARNING"), $formElt),
+            array("value" => _T("The user password has expired", "samba"))
+        );
+    }
 
     $checked = "";
-    $smbuser = False;
-    if (isset($ldapArr["uid"][0])) {
-        $smbuser = hasSmbAttr($ldapArr["uid"][0]);
-        if ($smbuser) {
-            if (!isEnabledUser($ldapArr["uid"][0])) {
-                $checked = "checked";
-            }
-        }
-    }
-
-    if ($smbuser) {
-        if (userPasswdHasExpired($ldapArr["uid"][0])) {
-            $formElt = new HiddenTpl("userPasswdHasExpired");
-            $f->add(
-                new TrFormElement(_("WARNING"), $formElt),
-                array("value" => _T("The user password has expired", "samba"))
-            );
-        }
-    }
-
+    if (($hasSmb && !isEnabledUser($uid)) || $FH->getArrayOrPostValue('isSmbDesactive') == 'on') $checked = "checked";
     $f->add(
         new TrFormElement(_T("User is disabled, if checked","samba"), new CheckboxTpl("isSmbDesactive"),
             array("tooltip" => _T("Disable samba user account",'samba'))),
         array ("value" => $checked)
     );
 
-    if ($smbuser) {
-        $checked = "";    
-        if (isLockedUser($ldapArr["uid"][0])) $checked = "checked";
-    }
+    $checked = "";
+    if (($hasSmb && isLockedUser($uid)) || $FH->getArrayOrPostValue('isSmbLocked') == 'on') $checked = "checked";
     $f->add(
         new TrFormElement(_T("User is locked, if checked","samba"), new CheckboxTpl("isSmbLocked"),
             array("tooltip" => _T("Lock samba user access<p>User can be locked after too many failed log.</p>",'samba'))),
         array ("value" => $checked)
     );
-    
+
     # display this options only if we are PDC
     if($smbInfo["pdc"]) {
         # if no global profile set, we can set a roaming profile for this user
         if(!$smbInfo["logon path"]) {
             $hasProfile = false;
-            $checked = "";    
-            if(isset($ldapArr["sambaProfilePath"])) {
+            $checked = "";
+            $value = "";
+            if($FH->getArrayOrPostValue("sambaProfilePath")) {
                 $hasProfile = true;
                 $checked = "checked";
+                $value = $FH->getArrayOrPostValue('sambaProfilePath');
             }
             $f->add(
                 new TrFormElement(_T("Use network profile, if checked","samba"), new CheckboxTpl("hasProfile")),
                 array ("value" => $checked, "extraArg" => 'onclick="toggleVisibility(\'pathdiv\')"')
             );
-            
+
             $f->pop();
-            
+
             $pathdiv = new Div(array('id' => 'pathdiv'));
             $pathdiv->setVisibility($hasProfile);
-            
+
             $f->push($pathdiv);
             $f->push(new Table());
 
-            $value = "\\\\".$smbInfo["netbios name"]."\\profiles\\".$ldapArr["uid"][0];
-            if(isset($ldapArr["sambaProfilePath"][0])) $value = $ldapArr["sambaProfilePath"][0];
             $f->add(
                 new TrFormElement(_T("Network path for user's profile","samba"), new InputTpl("sambaProfilePath")),
                 array ("value" => $value)
             );
-            
+
             $f->pop();
             $f->pop();
             $f->push(new Table());
-            
+
         }
 
         $checked = "";
-        if(!isset($ldapArr["sambaPwdCanChange"]) or $ldapArr["sambaPwdCanChange"][0] < mktime()) $checked = "checked";
+        if(!$FH->getArrayOrPostValue('sambaPwdCanChange') or $FH->getArrayOrPostValue('sambaPwdCanChange') < mktime())
+            $checked = "checked";
         $f->add(
             new TrFormElement(_T("User can change password, if checked","samba"), new CheckboxTpl("sambaPwdCanChange")),
             array ("value" => $checked)
         );
-        
+
         $checked = "";
-        if($ldapArr["sambaPwdLastSet"][0] == "0") $checked = "checked";
+        if($FH->getArrayOrPostValue('sambaPwdLastSet') == "0" || $FH->getArrayOrPostValue('sambaPwdLastSet') == "on") $checked = "checked";
         $f->add(
             new TrFormElement(_T("User must change password on next logon, <br/>if checked","samba"), new CheckboxTpl("sambaPwdLastSet")),
             array ("value" => $checked)
         );
 
         $value = "";
-        if(isset($ldapArr["sambaKickoffTime"][0])) $value = strftime("%Y-%m-%d %H:%M:%S", $ldapArr["sambaKickoffTime"][0]);
+        if($FH->getArrayOrPostValue('sambaKickoffTime'))
+            $value = strftime("%Y-%m-%d %H:%M:%S", $FH->getArrayOrPostValue('sambaKickoffTime'));
         $f->add(
             new TrFormElement(_T("Account expiration","samba"), new DynamicDateTpl("sambaKickoffTime"),
                 array("tooltip" => _T("Specifies the date when the user will be locked down and cannot login any longer. If this attribute is omitted, then the account will never expire.",'samba'))),
@@ -386,19 +359,17 @@ function _samba_baseEdit($ldapArr,$postArr) {
                    _T("Connect base directory on network drive","samba") => "sambaHomeDrive");
 
         foreach ($d as $description => $field) {
-            $value = "";
-            if (isset($ldapArr[$field][0])) $value = $ldapArr[$field][0];
             $f->add(
                 new TrFormElement($description, new InputTpl($field)),
-                array("value" => $value)
+                array("value" => $FH->getArrayOrPostValue($field))
             );
         }
-        
+
         $f->pop();
         $f->pop();
-        $f->pop();    
+        $f->pop();
     }
-    
+
     $f->display();
 
 }
