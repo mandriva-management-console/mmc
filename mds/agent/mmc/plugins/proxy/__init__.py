76a0dab8a66ed2582a7b9e6927df37c324c88e36
Jean-Philippe Braun <jpbraun@mandriva.com>
2012-10-09 10:11:08 +0200

    mds: bump version to 2.4.3.1

--- mds/agent/mmc/plugins/proxy/__init__.py
+++ mds/agent/mmc/plugins/proxy/__init__.py
@@ -35,7 +35,7 @@ from mmc.core.audit import AuditFactory as AF
 from mmc.plugins.proxy.audit import AT, AA, PLUGIN_NAME
 
 
-VERSION = "2.4.3"
+VERSION = "2.4.3.1"
 APIVERSION = "1:1:0"
 REVISION = scmRevision("$Rev$")
 
