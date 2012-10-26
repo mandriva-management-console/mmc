92a918b28c75882327fdb1858266eea502e49587
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2012-10-26 12:32:47 +0200

    pulse2: move menu_action.php to pulse2 module
        * move menu_action.php to pulse2 module
        * In a computer view don't display top right shortcut (VNC,
        * imaging...)
        if associated module is not enabled

--- pulse2/web/modules/pulse2/includes/menu_action.php
+++ pulse2/web/modules/pulse2/includes/menu_action.php
@@ -27,37 +27,60 @@ if ($_GET['cn']) $_SESSION['cn'] = $_GET['cn'];
 if ($_GET['objectUUID']) $_SESSION['objectUUID'] = $_GET['objectUUID'];
 if ($_GET['action']) $_SESSION['action'] = $_GET['action'];
 
-$paramArray = array('vnc' => '', 'cn' => $_SESSION['cn'], 'objectUUID' => $_SESSION['objectUUID']);
+$paramArray = array('cn' => $_SESSION['cn'], 'objectUUID' => $_SESSION['objectUUID']);
 
-
-$inventAction = new ActionItem(_("Inventory"),"invtabs","inventory","inventory", "base", "computers");
-$vncClientAction = new ActionItem(_("Remote control"), $_SESSION['action'], "vncclient", "computer", "base", "computers");
-$logAction = new ActionItem(_("Read log"),"msctabs","logfile","computer", "base", "computers", "tablogs");
-$mscAction = new ActionItem(_("Software deployment"),"msctabs","install","computer", "base", "computers");
-$imgAction = new ActionItem(_("Imaging management"),"imgtabs","imaging","computer", "base", "computers");
-
+$inventAction = new ActionItem(_T("Inventory", "pulse2"),"invtabs","inventory","inventory", "base", "computers");
+$vncClientAction = new ActionItem(_T("Remote control", "pulse2"), $_SESSION['action'] . "&vnc=", "vncclient", "computer", "base", "computers");
+$logAction = new ActionItem(_T("Read log", "pulse2"),"msctabs","logfile","computer", "base", "computers", "tablogs");
+$mscAction = new ActionItem(_T("Software deployment", "pulse2"),"msctabs","install","computer", "base", "computers");
+$imgAction = new ActionItem(_T("Imaging management", "pulse2"),"imgtabs","imaging","computer", "base", "computers");
 
 echo '<div style="float: right; width: 200px;">';
 echo '<table border="0" style="border: none;" cellpadding="0" cellspacing="0"><tr>';
     
 $actions = array($inventAction, $vncClientAction, $logAction, $mscAction, $imgAction);
+
+/*
+ * This function return True if action param is in enabled pulse modules
+ *
+ * @param string $action an action
+ * @return bool
+ */
+
+function modIsActive($action) {
+    $modActionAssoc = array(
+        "img" => "imaging",
+        "inv" => "inventory",
+        "vnc" => "msc",
+        "msc" => "msc",
+    );
+    $action = strtolower($action);
+    foreach ($modActionAssoc as $key => $value) {
+        $mod = '';
+        if (strpos($action, $key) !== false) {
+            $mod = $value;
+        }
+        if (in_array($mod, $_SESSION["supportModList"])) {
+            return True;
+        }
+    }
+
+    return False;
+}
+
 foreach ($actions as $action){
-	echo '<td style="border: none">';    
+        echo '<td style="border: none">';    
         if (is_array($paramArray)) {
-	   $paramArray['mod'] = $action->mod;
+            $paramArray['mod'] = $action->mod;
         }    
         echo "<li class=\"".$action->classCss."\" style=\"list-style-type: none; border: none; float:left; \" >";
         if (is_array($paramArray) & !empty($paramArray)) $urlChunk = $action->buildUrlChunk($paramArray);
         else $urlChunk = "&amp;" . $action->paramString."=" . rawurlencode($paramArray);
-	if ($action->action == "vnc_client"){
-            echo "<a title=\"".$action->desc."\" href=\"main.php?module=".$action->module."&amp;submod=".$action->submod."&amp;action=" . $action->action . $urlChunk . "\"";
-            echo " onclick=\"PopupWindow(event,'main.php?module=".$action->module."&amp;submod=".$action->submod."&amp;action=" .$action->action . $urlChunk . "', " . $action->width . "); return false;\">&nbsp;</a>"; 
-	}
-	else {
-	    echo "<a title=\"".$action->desc."\" href=\"" . urlStr($action->path) . $urlChunk . "\">&nbsp;</a>";
-	}
+        if (modIsActive($action->action)) {
+            echo "<a title=\"".$action->desc."\" href=\"" . urlStr($action->path) . $urlChunk . "\">&nbsp;</a>";
+        }
         echo "</li>";
-	echo '</td>';
+        echo '</td>';
 }
 echo '</tr></table>';	    
 echo '</div>';
