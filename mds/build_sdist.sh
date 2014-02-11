9138c6298be20cc13545fd4b22e871c536836e4d
Jean-Philippe Braun <jpbraun@mandriva.com>
2014-02-11 18:22:39 +0100

    core, mds: use make distcheck for tarballs

--- mds/build_sdist.sh
+++ mds/build_sdist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f mds-*.tar.gz mds-*.tar.gz.md5
-git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make dist
+git clean -fdx && ./autogen.sh && ./configure --sysconfdir=/etc --localstatedir=/var --disable-python-check --disable-conf && make distcheck
 if [ $? -eq 0 ]; then
     for tarball in mds-*.tar.gz; do
         md5sum $tarball > $tarball.md5
