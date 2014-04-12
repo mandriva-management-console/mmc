76f119ed4abffe714f57bf2fed847658bdd40ab9
Miguel Julián <mjulian@zentyal.com>
2014-04-12 02:16:35 +0200

    Enabled redirection if samba4 is already provisioned and form is requested

--- mds/web/modules/samba4/domaincontroller/provision.php
+++ mds/web/modules/samba4/domaincontroller/provision.php
@@ -25,12 +25,13 @@
  *   Miguel Julián <mjulian@zentyal.com>
  */
 
+require_once("modules/samba4/includes/common-xmlrpc.inc.php");
 require("modules/samba4/includes/domaincontroller-xmlrpc.inc.php");
 require("modules/samba4/mainSidebar.php");
 require("graph/navbar.inc.php");
 
 /* Provision has been ordered, just handle it or show the form */
-if (isset($_POST["bprovision"])) {
+if (isset($_POST["bprovision"]) and ! isSamba4Provisioned()) {
     if (_doProvision($_POST)) {
         header("Location: " . urlStrRedirect("base/main/default"));
         exit;
@@ -43,6 +44,8 @@ if (isset($_POST["bprovision"])) {
 _showProvisionForm($sidemenu);
 
 function _showProvisionForm($sidemenu) {
+    _redirectIfAlreadyProvisioned();
+
     $page = new PageGenerator(_T("Samba provisioning"));
     $page->setSideMenu($sidemenu);
     $page->display();
@@ -72,6 +75,8 @@ function _showProvisionForm($sidemenu) {
     $form->display();
 }
 
+/* Private functions */
+
 function _doProvision($_POST) {
     if (isset($_POST["domainName"])) {
         $domainName = $_POST["domainName"];
@@ -86,6 +91,13 @@ function _doProvision($_POST) {
     }
 
     return provisionSamba4($domainName, $realm);
+}
+
+function _redirectIfAlreadyProvisioned() {
+    if (isSamba4Provisioned()) {
+        header("Location: " . urlStrRedirect("samba4/shares/index"));
+        exit;
+    }
 }
 
 ?>
