00202d298695290306c809ccba3ae9ca61f42a40
Jesús García Sáez <jgarcia@zentyal.com>
2014-06-17 16:23:23 +0200

    s4sync: daemon mode won't have neither stdout nor stderr

--- mds/s4sync/s4sync.py
+++ mds/s4sync/s4sync.py
@@ -9,8 +9,8 @@ class S4SyncApp(object):
 
     def __init__(self, logger):
         self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
+        self.stdout_path = '/dev/null'
+        self.stderr_path = '/dev/null'
         self.pidfile_path =  '/var/run/s4sync.pid'
         self.pidfile_timeout = 5
 
