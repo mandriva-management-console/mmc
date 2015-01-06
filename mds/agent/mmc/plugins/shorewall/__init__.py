ad2abd189733da2b7908b607e43081808c5cf53d
Jean-Philippe Braun <eon@patapon.info>
2015-01-06 15:35:20 +0100

    mds: bump version to 2.5.86

--- mds/agent/mmc/plugins/shorewall/__init__.py
+++ mds/agent/mmc/plugins/shorewall/__init__.py
@@ -30,7 +30,7 @@ from mmc.support.mmctools import ServiceManager
 from mmc.plugins.shorewall.io import ShorewallConf, ShorewallLineInvalid
 from mmc.plugins.shorewall.config import ShorewallPluginConfig
 
-VERSION = "2.5.85"
+VERSION = "2.5.86"
 APIVERSION = "6:2:4"
 REVISION = scmRevision("$Rev$")
 
--- mds/agent/mmc/plugins/userquota/__init__.py
+++ mds/agent/mmc/plugins/userquota/__init__.py
@@ -40,7 +40,7 @@ from string import Template
 
 INI = mmcconfdir + "/plugins/userquota.ini"
 
-VERSION = "2.5.85"
+VERSION = "2.5.86"
 APIVERSION = "0:0:0"
 REVISION = scmRevision("$Rev$")
 
