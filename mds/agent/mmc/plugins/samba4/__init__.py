3884bc3ab68bd732c9663327674da3338202f567
Jean-Philippe Braun <eon@patapon.info>
2015-01-21 09:46:34 +0100

    mds: bump version to 2.5.89

--- mds/agent/mmc/plugins/samba4/__init__.py
+++ mds/agent/mmc/plugins/samba4/__init__.py
@@ -42,7 +42,7 @@ from mmc.support.mmctools import shlaunchBackground, progressBackup
 
 logger = logging.getLogger()
 
-VERSION = "2.5.88"
+VERSION = "2.5.89"
 APIVERSION = "1.0.0"
 REVISION = scmRevision("$Rev$")
 
--- mds/agent/mmc/plugins/shorewall/__init__.py
+++ mds/agent/mmc/plugins/shorewall/__init__.py
@@ -30,7 +30,7 @@ from mmc.support.mmctools import ServiceManager
 from mmc.plugins.shorewall.io import ShorewallConf, ShorewallLineInvalid
 from mmc.plugins.shorewall.config import ShorewallPluginConfig
 
-VERSION = "2.5.88"
+VERSION = "2.5.89"
 APIVERSION = "6:2:4"
 REVISION = scmRevision("$Rev$")
 
