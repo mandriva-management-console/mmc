f71236e1b2c7c66060acac61ee4ee2127cc6ff8a
Jean-Philippe Braun <eon@patapon.info>
2015-01-29 14:21:43 +0100

    mds: bump version to 2.5.91

--- mds/agent/mmc/plugins/bulkimport/__init__.py
+++ mds/agent/mmc/plugins/bulkimport/__init__.py
@@ -28,7 +28,7 @@ from mmc.site import mmcconfdir
 
 INI = mmcconfdir + "/plugins/bulkimport.ini"
 
-VERSION = "2.5.90"
+VERSION = "2.5.91"
 APIVERSION = "0:0:0"
 REVISION = scmRevision("$Rev$")
 
--- mds/agent/mmc/plugins/samba/__init__.py
+++ mds/agent/mmc/plugins/samba/__init__.py
@@ -39,7 +39,7 @@ from mmc.plugins.samba.smb_ldap import SambaLDAP
 
 logger = logging.getLogger()
 
-VERSION = "2.5.90"
+VERSION = "2.5.91"
 APIVERSION = "5:3:4"
 REVISION = scmRevision("$Rev$")
 
