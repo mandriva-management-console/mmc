86d8c472077e1cdfee8d8429ab2f07fd13b5e6ff
Jean-Christophe VASSORT <jcvassort@mandriva.com>
2015-02-06 15:02:37 +0100

    update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -5,18 +5,15 @@
 # ln -s ../../run-tests.sh .git/hooks/pre-commit
 
 # get the list of the files to checkout
-files=$(git diff-index --name-only --cached HEAD)
+
+#git diff-index --name-only --cached HEAD
+files="$(git diff-index --name-only --cached HEAD)"
 error="0"
+
 echo $files |grep '\.py'|xargs pyflakes || error=1
 echo $files |grep '\.sh'|xargs sh -n || error=1
-echo $files |while read file; do
-  if [ ! -s $file ]; then
-    error=1
-    echo "File $file is empty, please check it"
-  fi
+for file in $files; do
+  [ ! -s "$file" ] && error=1 && echo "File $file is empty, please check it"
 done
 
-if [ "$error" -eq "1" ]; then
-   exit 1
-fi
-exit 0
+exit $error
